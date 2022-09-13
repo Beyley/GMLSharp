b1e4da2141d0ca19e74b827439fe2ed72d3c25a7
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-09-13 16:01:12 -0700

    Lexer: Initial implementation

---
+++ GMLSharp.Test/Program.cs
@@ -0,0 +1,35 @@
+﻿using GMLSharp;
+
+public static class Program {
+	public static void Main(string[] args) {
+		Lexer lexer = new Lexer(@"// A 20x200 coolbar button with text.
+@GUI::Button {
+    width: 200
+    height: 20
+    text: ""Operation failed successfully.\""
+    button_style: ""Coolbar""
+}
+
+// Two tabs, named ""Tab 1"" and ""Tab 2"", each containing a label.
+@GUI::TabWidget {
+    min_width: 150
+    min_height: 200
+
+    @GUI::Label {
+        title: ""Tab 1""
+        text: ""This is the first tab""
+    }
+
+    @GUI::Label {
+        title: ""Tab 2""
+        text: ""This is the second tab. What did you expect?""
+    }
+}");
+        
+        lexer.Lex();
+        
+        foreach (Lexer.Token token in lexer.Tokens) {
+            Console.WriteLine(token);
+        }
+	}
+}
---
+++ GMLSharp/Lexer.cs
@@ -0,0 +1,186 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace GMLSharp;
+
+public class Lexer {
+	public struct Position {
+		public int Line;
+		public int Column;
+	}
+
+	public enum TokenType {
+		Unknown,
+		Comment,
+		ClassMarker,
+		ClassName,
+		LeftCurly,
+		RightCurly,
+		Identifier,
+		Colon,
+		JsonValue
+	}
+
+	public struct Token {
+		public TokenType Type = TokenType.Unknown;
+		public string    View;
+		public Position  Start;
+		public Position  End;
+
+		public override string ToString() {
+			return this.Type.ToString();
+		}
+
+		public Token() {
+			this.View  = "";
+			this.Start = new Position();
+			this.End   = new Position();
+		}
+	}
+
+	private readonly string _input;
+
+	private int      _index;
+	private Position _position;
+
+	public readonly List<Token> Tokens;
+
+	public Lexer(string input) {
+		this._input = input.Replace("\r\n", "\n");
+
+		this.Tokens = new List<Token>();
+	}
+
+	public char Peek(long offset = 0) {
+		if (this._index + offset >= this._input.Length)
+			return (char)0;
+
+		return this._input[(int)(this._index + offset)];
+	}
+
+	public char Consume() {
+		Debug.Assert(this._index < this._input.Length);
+
+		char ch = this._input[this._index++];
+		
+		if (ch == '\n') {
+			this._position.Line++;
+			this._position.Column = 0;
+		}
+		else {
+			this._position.Column++;
+		}
+
+		return ch;
+	}
+
+	private static bool IsValidIdentifierStart(char ch) {
+		return char.IsLetter(ch) || ch == '_';
+	}
+
+	private static bool IsValidIdentifierCharacter(char ch) {
+		return char.IsLetterOrDigit(ch) || ch == '_';
+	}
+
+	private static bool IsValidClassCharacter(char ch) {
+		return char.IsLetterOrDigit(ch) || ch is '_' or ':';
+	}
+
+	public void Lex() {
+		int      tokenStartIndex    = 0;
+		Position tokenStartPosition = new();
+
+		void BeginToken() {
+			tokenStartIndex    = this._index;
+			tokenStartPosition = this._position;
+		}
+
+		void CommitToken(TokenType type) {
+			Token token;
+			token.View  = this._input.Substring(tokenStartIndex, this._index - tokenStartIndex);
+			token.Type  = type;
+			token.Start = tokenStartPosition;
+			token.End   = this._position;
+			this.Tokens.Add(token);
+		}
+
+		void ConsumeClass() {
+			BeginToken();
+			this.Consume();
+			CommitToken(TokenType.ClassMarker);
+			BeginToken();
+			while (IsValidClassCharacter(this.Peek()))
+				this.Consume();
+			CommitToken(TokenType.ClassName);
+		}
+
+		while (this._index < this._input.Length) {
+			if (this.Peek() == ' ') {
+				BeginToken();
+				while (this.Peek() == ' ')
+					this.Consume();
+				continue;
+			}
+
+			// C++ style comments
+			if (this.Peek() != 0 && this.Peek() == '/' && this.Peek(1) == '/') {
+				BeginToken();
+				while (this.Peek() != 0 && this.Peek() != '\n')
+					this.Consume();
+				CommitToken(TokenType.Comment);
+				continue;
+			}
+
+			if (this.Peek() == '{') {
+				BeginToken();
+				this.Consume();
+				CommitToken(TokenType.LeftCurly);
+				continue;
+			}
+
+			if (this.Peek() == '}') {
+				BeginToken();
+				this.Consume();
+				CommitToken(TokenType.RightCurly);
+				continue;
+			}
+
+			if (this.Peek() == '@') {
+				ConsumeClass();
+				continue;
+			}
+
+			if (IsValidIdentifierStart(this.Peek())) {
+				BeginToken();
+				this.Consume();
+				while (IsValidIdentifierCharacter(this.Peek()))
+					this.Consume();
+				CommitToken(TokenType.Identifier);
+				continue;
+			}
+
+			if (this.Peek() == ':') {
+				BeginToken();
+				this.Consume();
+				CommitToken(TokenType.Colon);
+
+				while (this.Peek() == ' ')
+					this.Consume();
+
+				if (this.Peek() == '@') {
+					ConsumeClass();
+				}
+				else {
+					BeginToken();
+					while (this.Peek() != 0 && this.Peek() != '\n')
+						this.Consume();
+					CommitToken(TokenType.JsonValue);
+				}
+				continue;
+			}
+
+			this.Consume();
+			CommitToken(TokenType.Unknown);
+		}
+	}
+}
